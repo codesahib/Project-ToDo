036aae53d1a1e15f52077b866b8284d055835182
codesahib <gursahib.kvm@gmail.com>
2021-06-23 17:16:56 +0530

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,34 @@
+import './App.css';
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Project from './components/Project'
+
+function App() {
+  let i=1;
+  var projects_list = [];
+
+  for(i=1;i<=5;i++){
+    projects_list.push(<Project index={i}/>)
+  }
+
+  function add_project(){
+    console.log("Button clicked");
+    // projects_list.push(<Project index={i}/>);
+    // i++;
+  }
+  return (
+    <>
+    <div className="App">
+      <h1>Project-ToDo</h1>
+    </div>
+    <Header/>
+    {projects_list}
+    <center>
+      <button id="buttonAddProject" onClick={add_project}>Add Project</button>
+    </center>
+    <Footer/>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Project.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Project(props) {
+    return (
+        <div>
+            Project Component {props.index}
+        </div>
+    )
+}
