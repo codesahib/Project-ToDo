073aff697dd9534a8e6118196ae3cacd74153538
codesahib <gursahib.kvm@gmail.com>
2021-06-23 21:49:02 +0530

    [2021-06-23] Adding lib for fonts + Adding css for projects component + Adding elements in project component

--- frontend/src/components/Project.js
+++ frontend/src/components/Project.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import './Project.css'
 
 export default function Project(props) {
     return (
-        <div>
-            Project Component {props.index}
+        <div className="containerMain">
+            <input type="checkbox"></input>
+            <h5 id="projectTitle">Project Component {props.index}</h5>
+            <div className="projectOptionsList">
+                <i className="fa fa-gear projectOption"></i>
+                <i className="fa fa-list-alt projectOption"></i>
+                <i className="fa fa-trash projectOption"></i>
+                <i className="fa fa-chevron-down projectOption"></i>
+            </div>
         </div>
     )
 }
