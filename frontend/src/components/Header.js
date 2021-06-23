6ddc28b40bf94c487aa935bebf5c05546a4d4425
codesahib <gursahib.kvm@gmail.com>
2021-06-23 21:46:31 +0530

    [2021-06-23] Adding 'Login' button + Change navbar-brand + removed unused elements

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -4,7 +4,7 @@ export default function Header() {
     return (
         <div>
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <a class="navbar-brand" href="#">Project ToDo</a>
+                <a class="navbar-brand" href="#">Project-ToDo</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
@@ -17,23 +17,14 @@ export default function Header() {
                     <li class="nav-item">
                         <a class="nav-link" href="#">Link</a>
                     </li>
-                    {/* <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Dropdown
-                        </a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="#">Action</a>
-                        <a class="dropdown-item" href="#">Another action</a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="#">Something else here</a>
-                        </div>
-                    </li> */}
                     
                     </ul>
                     <form class="form-inline my-2 my-lg-0">
                         <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
                         <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
                     </form>
+                    &nbsp;&nbsp;
+                    <button class="btn btn-info my-2 my-sm-0">Login</button>
                 </div>
                 </nav>
         </div>
