2449fe6be8250c9769e1b1e88f1728eeb6ef3db6
codesahib <gursahib.kvm@gmail.com>
2021-06-25 01:15:14 +0530

    [2021-06-25] Text will be displayed if there are no projects in container + adding styles as object

--- frontend/src/components/ProjectContainer.js
+++ frontend/src/components/ProjectContainer.js
@@ -2,10 +2,14 @@ import React from 'react'
 import Project from './Project'
 
 export default function ProjectContainer({projects,delete_project,add_project}) {
+    let textNoProjectsStyle = {
+        padding: "20px"
+    }
+
     return (
         <div>
             <center>
-                {projects.map((project)=> {
+                { projects.length ===0? <h2 id="textNoProjects" style={textNoProjectsStyle}>No Projects Recorded</h2> :projects.map((project)=> {
                     return(
                         <Project project={project} key={project.id} delete_project={delete_project}/>
                     )
