afb58a008d480bb4616cf065bf5ea42927bafde1
codesahib <gursahib.kvm@gmail.com>
2021-06-25 23:20:05 +0530

    [2021-06-25] Minor changes to footer component

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -3,7 +3,7 @@ import './Footer.css'
 
 export default function Footer() {
     return (
-        <div className="mainContainer">
+        <div className="footerContainer">
             Footer Component
         </div>
     )
