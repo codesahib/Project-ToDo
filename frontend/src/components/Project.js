993935713535d069b94c83931a3107f551d057a3
codesahib <gursahib.kvm@gmail.com>
2021-06-26 02:09:56 +0530

    [2021-06-26] Using tasks component in project component

--- frontend/src/components/Project.js
+++ frontend/src/components/Project.js
@@ -1,26 +1,26 @@
 import React, { useState } from 'react';
 import './Project.css'
 import ChangeSettings from './Modals/ChangeSettings'
+import TaskContainer from './TaskContainer'
 
 export default function Project({project,delete_project}) {
     const [settingModal, setSettingModal] = useState(false);
+    const [taskContainer, setTaskContainer] = useState(false);
 
-    const change_desc = (project) => {
-        setSettingModal(true);
-    }
     return (
         <>
         {settingModal && <ChangeSettings show_settings={setSettingModal} project={project}/> }
-        <div className="containerMain">
+        <div id="projectDisplay">
             <input type="checkbox"></input>
             <h5 id="projectTitle">{project.title}</h5>
             <div className="projectOptionsList">
-                <i className="fa fa-gear projectOption" onClick={()=>{change_desc(project)}}></i>
+                <i className="fa fa-gear projectOption" onClick={()=>{setSettingModal(true)}}></i>
                 <i className="fa fa-list-alt projectOption"></i>
                 <i className="fa fa-trash projectOption" onClick={()=>{delete_project(project)}}></i>
-                <i className="fa fa-chevron-down projectOption"></i>
+                <i className="fa fa-chevron-down projectOption" onClick={()=>{setTaskContainer(!taskContainer)}}></i>
             </div>
         </div>
+        {taskContainer && <TaskContainer project={project}/> }
         </>
     )
 }
