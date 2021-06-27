ec4a6ec0949c96003dd8f267225de7b5bcd1cc5e
codesahib <gursahib.kvm@gmail.com>
2021-06-28 01:53:41 +0530

    [2021-06-28] Changing Project component theme and DOM

--- frontend/src/components/Project.js
+++ frontend/src/components/Project.js
@@ -11,17 +11,19 @@ export default function Project({project,delete_project}) {
     return (
         <>
         {settingModal && <ChangeSettings show_settings={setSettingModal} project={proj} updateProject={setProj}/> }
-        <div id="projectDisplay">
-            <input type="checkbox"></input>
-            <h5 id="projectTitle">{proj.title}</h5>
-            <div className="projectOptionsList">
-                <i className="fa fa-gear projectOption" onClick={()=>{setSettingModal(true)}}></i>
-                <i className="fa fa-list-alt projectOption"></i>
-                <i className="fa fa-trash projectOption" onClick={()=>{delete_project(proj)}}></i>
-                <i className="fa fa-chevron-down projectOption" onClick={()=>{setTaskContainer(!taskContainer)}}></i>
+        <div id="project">
+            <div id="projectSummary">
+                <input type="checkbox"></input>
+                <h5 id="projectTitle">{proj.title}</h5>
+                <div className="projectOptionsList">
+                    <i className="fa fa-gear projectOption" onClick={()=>{setSettingModal(true)}}></i>
+                    <i className="fa fa-list-alt projectOption"></i>
+                    <i className="fa fa-trash projectOption" onClick={()=>{delete_project(proj)}}></i>
+                    <i className="fa fa-chevron-down projectOption" onClick={()=>{setTaskContainer(!taskContainer)}}></i>
+                </div>
             </div>
+            {taskContainer && <TaskContainer project={proj}/> }
         </div>
-        {taskContainer && <TaskContainer project={proj}/> }
         </>
     )
 }
