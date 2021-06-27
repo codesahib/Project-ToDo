d93d37b6566729a0401f39354a0c87a5645e0f81
codesahib <gursahib.kvm@gmail.com>
2021-06-28 01:57:37 +0530

    [2021-06-28] Setting navbar theme and github repo link

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import './Header.css'
 
 export default function Header() {
     return (
         <div>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <nav className="navbar navbar-expand-lg navbar-light">
                 <a className="navbar-brand" href="#">Project-ToDo</a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
@@ -11,10 +12,12 @@ export default function Header() {
 
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav mr-auto">
-                    <li className="nav-item active">
-                        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-                    </li>
-                    
+                        <li className="nav-item active">
+                            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="https://github.com/codesahib/Project-ToDo/tree/master/frontend" target="_blank">GitHub Repo <span className="sr-only">(current)</span></a>
+                        </li>
                     </ul>
                     {/* <form className="form-inline my-2 my-lg-0">
                         <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
