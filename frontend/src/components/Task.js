34f0eeb65396bd5ba4c0823d7f23ef095c7beb6a
codesahib <gursahib.kvm@gmail.com>
2021-06-29 01:01:22 +0530

    [2021-06-29] Minor UI changes

--- frontend/src/components/Task.js
+++ frontend/src/components/Task.js
@@ -14,7 +14,7 @@ export default function Task({task,delete_task}) {
             <h5 id="taskTitle">{t.title}</h5>
             <div className="taskOptionsList">
                 <i className="fa fa-gear taskOption" onClick={()=>{setSettingModal(true)}}></i>
-                <i className="fa fa-list-alt taskOption"></i>
+                <i className="fa fa-sticky-note-o taskOption"></i>
                 <i className="fa fa-trash taskOption" onClick={()=>{delete_task(t)}}></i>
             </div>
         </div>
