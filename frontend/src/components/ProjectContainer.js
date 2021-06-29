a817fd582f4c1a8c6c5a18e2249ef7ecc0e12f05
codesahib <gursahib.kvm@gmail.com>
2021-06-30 01:53:38 +0530

    [2021-06-30] Adding external style sheet for ProjectContainer

--- frontend/src/components/ProjectContainer.js
+++ frontend/src/components/ProjectContainer.js
@@ -1,26 +1,17 @@
 import React from 'react'
 import Project from './Project'
 
-export default function ProjectContainer(props) {
-    let textNoProjectsStyle = {
-        "padding": "20px",
-        "text-align": "center"
-    }
+import './ProjectContainer.css'
 
-    let buttonAddProjectStyle= {
-        display: "flex",
-        "width": "fit-content",
-        "alignSelf": "center",
-        "backgroundColor": "#344765"
-    }
+export default function ProjectContainer(props) {
     return (
-        <div className="container" style={{display:"flex", "flexDirection":"column", "marginTop": "30px"}}>
-            { props.projects.length === 0? <h2 id="textNoProjects" style={textNoProjectsStyle}>No Projects Recorded</h2> : props.projects.map((project)=> {
+        <div id="projectContainer">
+            { props.projects.length === 0? <h2 id="textNoProjects">No Projects Recorded</h2> : props.projects.map((project)=> {
                 return(
                     <Project key={project.id} project={project} delete_project={props.delete_project}/>
                 )
             })}
-            <button style={buttonAddProjectStyle} id="buttonAddProject" className="btn btn-info" onClick={() => {props.show_modal(true)}}>+ Add Project</button>
+            <button id="buttonAddProject" className="btn btn-info" onClick={() => {props.show_modal(true)}}>+ Add Project</button>
         </div>
     )
 }
