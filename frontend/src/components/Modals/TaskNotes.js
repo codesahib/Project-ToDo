49361868def8c016f5678d4745b86a664ae57738
codesahib <gursahib.kvm@gmail.com>
2021-07-01 22:38:53 +0530

    [2021-07-01] Task Notes can be updated now

---
+++ frontend/src/components/Modals/TaskNotes.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import './TaskNotes.css'
+
+export default function ChangeSettings(props) {
+    const [notes, setNotes] = useState(props.notes);
+    
+    const submit = (e) => {
+        e.preventDefault(); // Avoid page reload
+        props.update_notes(notes);
+        props.show_notes(false)
+    }
+
+    return (
+        <div id="formContainer">
+            <form onSubmit={submit}>
+                <div className="form-group">
+                    <label htmlFor="notes">Task Notes</label>
+                    <textarea id="inputNotes" placeholder="Notes" rows="4" cols="50" value={notes} onChange={(e)=>{setNotes(e.target.value)}}></textarea>
+                </div>
+                <div className="form-group" id="buttonContainer">
+                    <button type="cancel" className="btn btn-danger formButton" onClick={()=>{props.show_notes(false)}}>Cancel</button>
+                    <button type="submit" className="btn btn-primary formButton">Update</button>
+                </div>
+            </form>
+        </div>
+        
+    )
+}
--- frontend/src/components/Task.js
+++ frontend/src/components/Task.js
@@ -1,10 +1,13 @@
 import React, { useState } from 'react';
 import './Task.css'
 import ChangeTaskSettings from './Modals/ChangeTaskSettings'
+import TaskNotes from './Modals/TaskNotes'
 
 export default function Task({task,delete_task}) {
     const [settingModal, setSettingModal] = useState(false);
+    const [showNotes, setShowNotes] = useState(false)
     const [t, setT] = useState(task);
+    const [tNotes, setTNotes] = useState(task.notes)
 
     return (
         <>
@@ -14,10 +17,11 @@ export default function Task({task,delete_task}) {
             <h5 id="taskTitle">{t.title}</h5>
             <div className="taskOptionsList">
                 <i className="fa fa-gear taskOption" onClick={()=>{setSettingModal(true)}}></i>
-                <i className="fa fa-sticky-note-o taskOption"></i>
+                <i className="fa fa-sticky-note-o taskOption" onClick={()=>{setShowNotes(true)}}></i>
                 <i className="fa fa-trash taskOption" onClick={()=>{delete_task(t)}}></i>
             </div>
         </div>
+        {showNotes && <TaskNotes show_notes={setShowNotes} notes={tNotes} update_notes={setTNotes}/>}
         </>
     )
 }
