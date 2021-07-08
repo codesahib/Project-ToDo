e489490be47a2f611aefe85a779f80be001196fc
codesahib <gursahib.kvm@gmail.com>
2021-07-09 02:54:23 +0530

    [2021-07-09] Added local DB for testing and added its path in .env + package install/update

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ dotenv.config()
 const port = process.env.PORT || 8000
 
 // Connecting DB
-mongoose.connect(process.env.RESTREVIEWS_DB_URI,{ useNewUrlParser: true })
+mongoose.connect(process.env.PROJECTS_DB_URI_DEV,{ useNewUrlParser: true })
 .then(async (client)=>{
     console.log("Database Connection Successful");
     await ProjectsDAO.injectDB(client);
@@ -22,28 +22,3 @@ mongoose.connect(process.env.RESTREVIEWS_DB_URI,{ useNewUrlParser: true })
     console.log("****** Error Connecting to DB. Error: "+ err)
     process.exit(1)
 })
-
-
-const createProject = async () => {
-    try {
-        const newProject = new Projects(
-            {
-                "title":"Complete MERN Project",
-                "description": "Hands-on experience on new tech stack",
-                "tasks": [
-                    {
-                        "title": "Learn Basics",
-                        "notes": "Haven't started"
-                    }
-                ]
-            }
-        )
-        const result = await newProject.save()
-        console.log(result)
-    }
-    catch(err){
-        console.log(err)
-    }
-}
-
-// createProject()
