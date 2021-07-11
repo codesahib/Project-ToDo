c5527e9a3051e4fa003baac2955ea3b1897bf1f1
codesahib <gursahib.kvm@gmail.com>
2021-07-11 14:08:00 +0530

    [2021-07-11] [Major Update]
    Adding routes, api func, DB calls for CRUD operations

--- backend/api/projects.controller.js
+++ backend/api/projects.controller.js
@@ -3,7 +3,7 @@ import ProjectsDAO from "../dao/projectsDAO.js"
 /* All the Data Manipulation takes place here */
 
 export default class ProjectsController {
-    static async apiGetProjects(req, res, next) {
+    static async apiGetProjects(req, res) {
         try{
             const projectsList = await ProjectsDAO.getProjects()
             res.json(projectsList)
@@ -13,7 +13,7 @@ export default class ProjectsController {
         }
     }
 
-    static async apiCreateProject(req, res, next) {
+    static async apiCreateProject(req, res) {
         const project_to_be_added = req.body
         console.log("[projects.controller][apiCreateProject] Request: \n" + JSON.stringify(project_to_be_added,null,2))
 
@@ -24,5 +24,46 @@ export default class ProjectsController {
         catch(err){
             console.log(`[projects.controller][apiCreateProject] Error: ${err}`)
         }
+    }
+
+    static async apiDeleteProject(req, res) {
+        const project_to_be_deleted = req.body
+        console.log("[projects.controller][apiDeleteProject] Request: \n" + JSON.stringify(project_to_be_deleted,null,2))
+
+        try{
+            await ProjectsDAO.deleteProject(project_to_be_deleted)
+            res.json({"Response":"Project deleted successfully"})
+        }
+        catch(err){
+            console.log(`[projects.controller][apiDeleteProject] Error: ${err}`)
+        }
+    }
+
+    static async apiAddTask(req,res) {
+
+        console.log("[projects.controller][apiAddTask] Request: \n" + JSON.stringify(req.body,null,2))
+
+        try{
+            await ProjectsDAO.addTask(req.body)
+            res.json({"Response":"Task added successfully"})
+        }
+        catch(err){
+            console.log(`[projects.controller][apiAddTask] Error: ${err}`)
+        }
+
+    }
+
+    static async apiRemoveTask(req,res) {
+
+        console.log("[projects.controller][apiRemoveTask] Request: \n" + JSON.stringify(req.body,null,2))
+
+        try{
+            await ProjectsDAO.removeTask(req.body)
+            res.json({"Response":"Task removed successfully"})
+        }
+        catch(err){
+            console.log(`[projects.controller][apiRemoveTask] Error: ${err}`)
+        }
+
     }
 }
--- backend/api/projects.route.js
+++ backend/api/projects.route.js
@@ -5,5 +5,9 @@ const router = express.Router()
 
 router.route("/").get(ProjectsCtrl.apiGetProjects)
 router.route("/create").post(ProjectsCtrl.apiCreateProject)
+router.route("/delete").post(ProjectsCtrl.apiDeleteProject)
+
+router.route("/add/task").post(ProjectsCtrl.apiAddTask)
+router.route("/remove/task").post(ProjectsCtrl.apiRemoveTask)
 
 export default router
